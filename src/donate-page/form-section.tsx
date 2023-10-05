5eedc5e562e5fc3503c5373bdc4313abfaaba1d0
Kevin Gray <kgray@twistbioscience.com>
2023-10-05 16:45:43 -0700

    Update form-section.tsx

--- src/donate-page/form-section.tsx
+++ src/donate-page/form-section.tsx
@@ -11,7 +11,6 @@ import React, { PropsWithChildren } from 'react'
 import update from 'immutability-helper';
 
 /* ****** Assets ****** */
-import transparentLogo from '../assets/images/transparent-white-3.png';
 
 /* ****** Constants ****** */
 // Imported
@@ -21,8 +20,8 @@ import transparentLogo from '../assets/images/transparent-white-3.png';
 // create a component
 const FormSection = () => {
     return (
-        <div className='px-4 py-10 bg-gradient-to-br from-purple to-tan'>
-            <img className='lg:w-1/4' src={transparentLogo} alt="california approves logo"/>
+        <div className='px-4 py-10 bg-gradient-to-br from-purple to-tan flex flex-col items-center'>
+            <script src="https://donorbox.org/widget.js" paypalExpress="false"></script><iframe src="https://donorbox.org/embed/california-approval-voting?default_interval=o&hide_donation_meter=true" name="donorbox" allowpaymentrequest="allowpaymentrequest" seamless={true} frameBorder="0" scrolling="no" height="900px" width="100%" style={{maxWidth: "500px", minWidth: "250px", maxHeight:"none!important"}}></iframe>
         </div>
     );
 }
