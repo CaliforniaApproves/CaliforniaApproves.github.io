2489ca44ac84b570fdf03b4a83aa1b79eacc5892
Alan Savage <asavageiv@gmail.com>
2024-02-03 15:04:57 -0600

    Remove unused base config option

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [reactRefresh(), svgr()]
-    , base: `/$viteBaseName/`
     , server: {
         fs: {
             allow: [searchForWorkspaceRoot(process.cwd()), '/node_modules/']
