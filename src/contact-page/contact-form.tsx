9d65c12339efda0c8d184181f7b405be61cb60db
Alan Savage <3028205+asavageiv@users.noreply.github.com>
2024-02-25 12:18:47 -0800

    Fix contact page

--- src/contact-page/contact-form.tsx
+++ src/contact-page/contact-form.tsx
@@ -28,54 +28,84 @@ const ContactForm = () => {
 
     return (
         <div className='py-16 text-center flex flex-col'>
-            <Logo className='w-40 m-auto'/>
+            <Logo className='w-40 m-auto' />
             <h2 className='m-auto'>Effect Change</h2>
-            <br/>
+            <br />
             <div className='text-left m-auto max-w-2xl'>
-                <h3>Contact Info</h3>
                 <p className='text-base'>
                     This is your first step to getting involved in Californian election reform. We will send you our discord link, our virtual meet up link, and an introductory email.
                 </p>
-                <div className='flex flex-row justify-between'>
-                    <div className='w-1/2 mr-4'>
-                        <h5>First Name</h5>
-                        <Input
-                            value={firstName}
-                            setValue={setFirstName}
-                            color='green'
-                        />
-                    </div>
-                    <div className='w-1/2 ml-4'>
-                        <h5>Last Name</h5>
-                        <Input
-                            value={lastName}
-                            setValue={setLastName}
-                            color='green'
-                        />
-                    </div>
-                </div>
-                <div className='flex flex-row justify-between'>
-                    <div className='w-1/2 mr-4'>
-                        <h5>Email Address</h5>
-                        <Input
-                            value={email}
-                            setValue={setEmail}
-                            color='green'
-                            type='email'
-                        />
-                        <p className='text-sm'>This will not be shared broadly or used to subscribe you to any lists.</p>
-                    </div>
-                    <ActionButton
-                        className='m-auto'
-                        variant='outlined'
-                        color='purple'
-                        onClick={() => {}}
+                <div id="mc_embed_signup" className='w-full'>
+                    <form
+                        action="https://californiaapproves.us5.list-manage.com/subscribe/post?u=b4aa7540a62457c043ff00e36&amp;id=dddf3d641c&amp;f_id=003abee6f0"
+                        method="post"
+                        id="mc-embedded-subscribe-form"
+                        name="mc-embedded-subscribe-form"
+                        className="validate"
+                        target="_self"
                     >
-                        Send
-                    </ActionButton>
+                        <div id="mc_embed_signup_scroll">
+                            <div id="mc-email-input-wrapper" className="mc-field-group">
+                                <Input
+                                    color="purple"
+                                    value={email}
+                                    setValue={setEmail}
+                                    label="email"
+                                    labelHidden={true}
+                                    type='email'
+                                    name="EMAIL"
+                                    placeholder='Enter your email'
+                                    className='w-full required email'
+                                    id="mce-EMAIL"
+                                    required
+                                />
+                                <span id="mce-EMAIL-HELPERTEXT" className="helper_text"></span>
+                            </div>
+                            <div id="mce-responses" className="clear">
+                                <div
+                                    className="response"
+                                    id="mce-error-response"
+                                ></div>
+                                <div
+                                    className="response"
+                                    id="mce-success-response"
+                                    style={{ display: "none" }}
+                                ></div>
+                            </div>
+                            {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
+                            <div style={{ position: "absolute", left: '-5000px' }} aria-hidden="true">
+                                <input
+                                    type="text"
+                                    name="b_b4aa7540a62457c043ff00e36_dddf3d641c"
+                                    tabIndex={-1}
+                                    value=""
+                                />
+                            </div>
+                            <div className="mt-4">
+                                <ActionButton
+                                    color='purple'
+                                    variant='outlined'
+                                    className="text-purple mx-auto"
+                                    size='lg'
+                                    name="subscribe"
+                                    id="mc-embedded-subscribe"
+                                    type="submit"
+                                >
+                                    JOIN US
+                                </ActionButton>
+                            </div>
+                        </div>
+                    </form>
                 </div>
+
+                <br />
+
+                <br />
+                <p className='italic text-small'>
+                    By subscribing you agree to with our Privacy Policy and provide consent to receive updates from our organization.
+                </p>
             </div>
-        </div>
+        </div >
     );
 }
 
